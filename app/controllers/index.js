5be161b8e49e7facf7ff737e3fb66c0bbbded267
sinanekiz <sinan.ekiz@hayenteknoloji.com>
2016-11-06 20:49:38 +0300

    signup

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 router.get('/', function (req, res) {
     res.render('index');
